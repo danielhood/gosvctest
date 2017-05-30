7570f20a85feb214b3d002d1ba2f9736ff0315e4
Daniel Hood <danielhood@hotmail.com>
2017-05-29 22:55:20 -0400

    Added serices

--- handlers/ping.go
+++ handlers/ping.go
@@ -2,18 +2,22 @@ package handlers
 
 import (
   "net/http"
+
+  "github.com/danielhood/gosvctest/services"
 )
 
-type Ping struct {}
+type Ping struct {
+  svc services.Ping
+}
 
 func NewPing() *Ping {
-  return &Ping{}
+  return &Ping{services.NewPing()}
 }
 
 func (h *Ping) ServeHTTP(w http.ResponseWriter, req *http.Request) {
     switch req.Method {
     case "GET":
-        s := "pong"
+        s := h.svc.Get()
         w.Write([]byte(s))
     default:
         http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
